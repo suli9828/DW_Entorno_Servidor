5add7e6ca85a1ee3a89edaa9f8e21dd0534a3ab8
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-10-04 10:23:23 +0200

    Add files via upload

--- Ejercicios/cantidad3/principal.php
+++ Ejercicios/cantidad3/principal.php
@@ -2,22 +2,32 @@
 <html lang="es-es">
 <head>
 <title>Array Pares</title>
-    <?php include 'funciones.php'; ?>
+    <?php require_once 'funciones.php'; ?>
 </head>
 <body>
-	<?php
-if (isset($_POST['enviar1'])) {
+<?php
+if(isset($_POST['enviar1'])){
     crearFormulario($_POST['cantidad']);
 } else if (isset($_POST['enviar2'])) {
     suma($_POST['cantidad']);
 } else {
-    ?>
-	<form action="funciones.php" method="post">
-	<p>Introduce un número.</p>
-		<input type="number" name="enviar1"> <input type="hidden" name="suma"
-			value="0"> <br> <input type="submit" value="Siguiente">
-	</form>
-	<?php
+?>
+    <div style="width: 50%; margin: auto; margin-top: 6%;">
+        <form action="principal.php" method="post">
+            <table style="width: 50%; margin: auto; text-align: center;">
+                <tr>
+                    <th>Introduce una cantidad: </th>
+                </tr>
+                <tr>
+                    <td><input type="number" name="cantidad" placeholder="Cantidad..."></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Enviar" name="enviar1"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+<?php
 }
 ?>
 </body>
