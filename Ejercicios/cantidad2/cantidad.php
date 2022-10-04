f7757cfee1841723c261ae868ae94716db8acbbe
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-10-04 10:03:20 +0200

    Create cantidad.php

---
+++ Ejercicios/cantidad2/cantidad.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es-es">
+<head>
+<title>Cantidad</title>
+</head>
+<body>
+	<form action="formulario.php" method="post">
+		<p>Introduce un número.</p>
+		<input type="number" name="enviar1"> <input type="hidden" name="suma"
+			value="0"> <br> <input type="submit" value="Siguiente">
+	</form>
+</body>
+</html>
