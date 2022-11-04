1d324c6127e2b4f87ae55f8edffa1dcd6da20900
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-11-04 13:12:37 +0100

    Create cabecera.php

---
+++ SQL/Instituto/includes/cabecera.php
@@ -0,0 +1,74 @@
+<?php require_once 'conexion.php'; ?>
+<?php require_once 'includes/helpers.php'; ?>
+<!DOCTYPE HTML>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>Instituto</title>
+		<link rel="stylesheet" type="text/css" href="./css/style.css" />
+	</head>
+	<body>
+		
+		<header id="cabecera">
+			<!-- LOGO -->
+			<div id="logo">
+				<a href="principalAlumno.php">
+					Instituto
+				</a>
+			</div>
+			
+
+			<nav id="menu">
+				<ul>
+					<li>
+						<a href="principalAlumno.php">Alumnos</a>
+					</li>
+					<li>
+						<a href="principalProfesor.php">Profesores</a>
+					</li>
+					<li>
+						<a href="principalGrupo.php">Grupos</a>
+					</li>
+					<li>
+						<a href="insertAlumno.php">A&ntilde;adir alumno</a>
+					</li>
+					<li>
+						<a href="deleteAlumno.php">Eliminar alumno</a>
+					</li>
+					<li>
+						<a href="updateAlumno.php">Editar alumno</a>
+					</li>
+					<li>
+						<a href="buscaAlumnoporid.php">Buscar alumno</a>
+					</li>
+					<li>
+						<a href="insertProfesor.php">A&ntilde;adir profesor</a>
+					</li>
+					<li>
+						<a href="deleteProfesor.php">Eliminar profesor</a>
+					</li>
+					<li>
+						<a href="updateProfesor.php">Editar profesor</a>
+					</li>
+					<li>
+						<a href="buscaProfesorporid.php">Buscar profesor</a>
+					</li>
+					<li>
+						<a href="insertGrupo.php">A&ntilde;adir grupo</a>
+					</li>
+					<li>
+						<a href="deleteGrupo.php">Eliminar grupo</a>
+					</li>
+					<li>
+						<a href="updateGrupo.php">Editar grupo</a>
+					</li>
+					<li>
+						<a href="buscaGrupoporid.php">Buscar grupo</a>
+					</li>
+				</ul>
+			</nav>
+			
+			
+		</header>
+		
+		<div id="contenedor">
