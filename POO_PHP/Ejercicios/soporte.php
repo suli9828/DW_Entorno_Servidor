d0e716fea382b71449b823c5c52bd3a3f90f77d3
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-11-22 08:39:32 +0100

    Create soporte.php

---
+++ POO_PHP/Ejercicios/soporte.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+class Soporte{
+  public string $titulo;
+  protected string $numero;
+  private float $precio;
+  private const IVA = 21;
+
+  public function __construct(string $titulo, string $numero, float $precio){
+    $this->titulo = $titulo;
+    $this->numero = $numero;
+    $this->precio = $precio;
+  }
+
+  public function getPrecio(){
+    return $this->precio;
+  }
+
+  public function getPrecioConIva(){
+    return $this->precio + ($this->precio * (self::IVA / 100));
+  }
+
+  public function Numero(){
+    return $this->numero;
+  }
+
+  public function muestraResumen(){
+    echo '<p>'.$this->titulo .'</br>'. $this->precio . '€ (IVA no incluido)</p>';
+  }
+
+}
+
+?>
