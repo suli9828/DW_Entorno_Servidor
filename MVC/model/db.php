8e4e70c8db00cad1cac24e58f74f9669147e7325
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-12-13 10:22:14 +0100

    Add files via upload

--- MVC/model/db.php
+++ MVC/model/db.php
@@ -1,1 +1,30 @@
+<?php
 
+    declare(strict_types=1);
+    include_once ('./config/config.php');
+
+    class DB {
+
+        private $host;
+        private $db;
+        private $user;
+        private $pass;
+        public $connection;
+
+
+        public function __construct(){
+            $this->host = constant('DB_HOST');
+            $this->db = constant('DB');
+            $this->user = constant('DB_USER');
+            $this->pass = constant('DB_PASS');
+        }
+
+        public function connect(): mysqli {
+            $connection = new mysqli($this->host, $this->user, $this->pass, $this->db);
+            if ($connection->connect_errno) {
+                die('Error de conexión: ' . $connection->connect_errno);
+            }
+            return $connection;
+        }
+    }
+?>
--- MVC/model/note.php
+++ MVC/model/note.php
@@ -1,1 +1,80 @@
+<?php
+    /* uso estricto */
+    declare(strict_types=1);
+    include_once 'db.php';
 
+    class Note{
+    
+        public function __construct(
+            public string $id,
+            public string $title,
+            public string $content,
+        ){}
+    }
+
+    class NoteTabla{
+
+        public function __construct(
+            private $tabla = 'note',
+            private $conexion = null,
+            private array $listaNotas = []
+        ){}
+
+        public function getListaNotas(){
+            return $this->listaNotas;
+        }
+
+        public function getConexion(){
+            $this->conexion = new DB();
+            return $this->conexion->connect();
+        }
+        public function getNotas(){
+            $sql = "SELECT * FROM $this->tabla";
+            $result = $this->getConexion()->query($sql);
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    $this->listaNotas[] = new Note(
+                        $row['id'],
+                        $row['title'],
+                        $row['content']
+                    );
+                }
+            }
+
+            return $this->listaNotas;
+        }
+
+        public function getNota($id){
+            $sql = "SELECT * FROM $this->tabla WHERE id = $id";
+            $result = $this->getConexion()->query($sql);
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    $this->listaNotas[] = new Note(
+                        $row['id'],
+                        $row['title'],
+                        $row['content']
+                    );
+                }
+            }
+
+            return $this->listaNotas;
+        }
+
+        public function insertarNota($title, $content){
+            $sql = "INSERT INTO $this->tabla (title, content) VALUES ('$title', '$content')";
+            $this->getConexion()->query($sql);
+        }
+        
+
+        public function eliminarNota($id){
+            $sql = "DELETE FROM $this->tabla WHERE id = $id";
+            $this->getConexion()->query($sql);
+        }
+
+        public function editarNota($id, $title, $content){
+            $sql = "UPDATE $this->tabla SET title = '$title', content = '$content' WHERE id = $id";
+            $this->getConexion()->query($sql);
+        }
+
+    }
+?>
