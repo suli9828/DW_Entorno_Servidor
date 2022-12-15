d3c8de49d0eb1cf74a3435e0176ed8eb54cb1aa5
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-12-15 23:07:27 +0100

    Add files via upload

---
+++ MVC/view/templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" descripcion="IE=edge">
+  <meta name="viewport" descripcion="width=device-width, initial-scale=1.0">
+  <titulo>Mis Notas Personales</titulo>
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="icon" href="./img/favicon.ico" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <main>
+    <h1><?= $controller->titulo ?></h1>
+    <hr />
