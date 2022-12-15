324260f1130e12bbead96e91ba6e7ec1ecc06e9d
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-12-15 23:06:00 +0100

    Add files via upload

--- MVC/model/db.php
+++ MVC/model/db.php
@@ -1,30 +1,47 @@
 <?php
 
-    declare(strict_types=1);
-    include_once ('./config/config.php');
+include_once 'config/config.php';
 
-    class DB {
+// Clase que se conecta con la Base de Datos.
+class DB{
 
-        private $host;
-        private $db;
-        private $user;
-        private $pass;
-        public $connection;
+  /* Atributos */
+  private string $servername; // Servidor.
+  private string $dbname; // Base de Datos.
+  private string $username; // Usuario.
+  private string $passwd; // Contraseña.
+  private $conection; // Conexión.
 
+  /* Constructor */
+  public function __construct(){
+    // Se inicializan los valores de la conexión mediante las constantes creadas en config.php.
+    $this->servername = constant("DB_HOST");
+    $this->username = constant("DB_USER");
+    $this->passwd = constant("DB_PASSWD");
+    $this->dbname = constant("DB");
 
-        public function __construct(){
-            $this->host = constant('DB_HOST');
-            $this->db = constant('DB');
-            $this->user = constant('DB_USER');
-            $this->pass = constant('DB_PASS');
-        }
-
-        public function connect(): mysqli {
-            $connection = new mysqli($this->host, $this->user, $this->pass, $this->db);
-            if ($connection->connect_errno) {
-                die('Error de conexión: ' . $connection->connect_errno);
-            }
-            return $connection;
-        }
+    // Se crea la conexión y se almacena en $this->conection.
+    $this->conection = new mysqli($this->servername, $this->username, $this->passwd, $this->dbname);
+    if($this->conection->connect_error){ // Si hay algún error.
+      die("Conection failed: ". $this->conection->error); // Cierra la conexión y devuelve un mensaje.
     }
+    
+  }
+
+  /* Métodos */
+
+  // Devuelve la conexión.
+  public function getConection(){
+    return $this->conection;
+  }
+
+  // Cierra la conexión.
+  public function closeConection(){
+    $this->conection->close();
+  }
+
+}
+
+
+
 ?>
--- MVC/model/note.php
+++ MVC/model/note.php
@@ -1,80 +1,136 @@
 <?php
-    /* uso estricto */
-    declare(strict_types=1);
-    include_once 'db.php';
 
-    class Note{
-    
-        public function __construct(
-            public string $id,
-            public string $title,
-            public string $content,
-        ){}
+include_once 'db.php';
+
+// Clase Nota.
+class Note{
+
+  /* Atributos */
+  private int $id;
+  private string $titulo;
+  private string $descripcion;
+
+  /* Constructor */
+  public function __construct(int $id, string $titulo, string $descripcion){
+    $this->id = $id;
+    $this->titulo = $titulo;
+    $this->descripcion = $descripcion;
+  }
+
+  /* Métodos */
+
+  // Getters y Setters.
+  public function getId(){
+    return $this->id;
+  }
+  public function setId(int $id){
+    $this->id = $id;
+  }
+
+  public function gettitulo(){
+    return $this->titulo;
+  }
+  public function settitulo(int $titulo){
+    $this->titulo = $titulo;
+  }
+
+  public function getdescripcion(){
+    return $this->descripcion;
+  }
+  public function setdescripcion(int $descripcion){
+    $this->descripcion = $descripcion;
+  }
+
+}
+
+// Clase nota que interactúa con la base de datos.
+class NoteTable{
+
+  /* Atributos */
+  private string $table = 'nota'; // Nombre de la tabla.
+  private $conection; // Conexión con la base de datos.
+  private array $listNotes = array(); // Array dónde se guardan todas las notas.
+
+  /* Constructor */
+  public function __construct(){}
+
+  /* Métodos */ 
+  
+  // Inicializar la conexión con la bbdd.
+  public function getConection(){
+    $conn = new DB(); // Crea un objeto DB.
+    $this->conection = $conn->getConection(); // Guarda en $this->conexion la conexión creada con el objeto DB.
+  }
+
+  // Accede la base de datos y guarda en $this->listNotes todas las notas que haya en la BBDD.
+  public function getNotes(){
+    $this->getConection(); // Inicializa la conexión.
+    $sql = "SELECT * FROM ". $this->table; // Consulta.
+    $result = $this->conection->query($sql); // Ejecuta la consulta.
+    if($result->num_rows > 0){ // Si devuelve más de 0 filas.
+      $i = 0; // Índice del array.
+      while($row = $result->fetch_assoc()){ // Se recorren las filas mientras sigan habiendo.
+        $this->listNotes[$i] = new Note($row['id'], $row['titulo'], $row['descripcion']); // Se crea un objeto nota y se guarda en el array.
+        $i++; // Aumenta el índice del array.
+      }
     }
+    $this->conection->close(); // Cierra la conexión.
+    return $this->listNotes; // Se devuelve el array.
+  }
 
-    class NoteTabla{
-
-        public function __construct(
-            private $tabla = 'note',
-            private $conexion = null,
-            private array $listaNotas = []
-        ){}
-
-        public function getListaNotas(){
-            return $this->listaNotas;
-        }
-
-        public function getConexion(){
-            $this->conexion = new DB();
-            return $this->conexion->connect();
-        }
-        public function getNotas(){
-            $sql = "SELECT * FROM $this->tabla";
-            $result = $this->getConexion()->query($sql);
-            if($result->num_rows > 0){
-                while($row = $result->fetch_assoc()){
-                    $this->listaNotas[] = new Note(
-                        $row['id'],
-                        $row['title'],
-                        $row['content']
-                    );
-                }
-            }
-
-            return $this->listaNotas;
-        }
-
-        public function getNota($id){
-            $sql = "SELECT * FROM $this->tabla WHERE id = $id";
-            $result = $this->getConexion()->query($sql);
-            if($result->num_rows > 0){
-                while($row = $result->fetch_assoc()){
-                    $this->listaNotas[] = new Note(
-                        $row['id'],
-                        $row['title'],
-                        $row['content']
-                    );
-                }
-            }
-
-            return $this->listaNotas;
-        }
-
-        public function insertarNota($title, $content){
-            $sql = "INSERT INTO $this->tabla (title, content) VALUES ('$title', '$content')";
-            $this->getConexion()->query($sql);
-        }
-        
-
-        public function eliminarNota($id){
-            $sql = "DELETE FROM $this->tabla WHERE id = $id";
-            $this->getConexion()->query($sql);
-        }
-
-        public function editarNota($id, $title, $content){
-            $sql = "UPDATE $this->tabla SET title = '$title', content = '$content' WHERE id = $id";
-            $this->getConexion()->query($sql);
-        }
-
+  // Accede a la base de datos para devolver la nota que cuyo id sea el que se le pase como parámetro.
+  public function getNoteById($id){
+    $this->getConection(); // Inicializa la conexión.
+    $sql = "SELECT * FROM ". $this->table ." WHERE id=$id"; // Consulta.
+    $result = $this->conection->query($sql); // Ejecuta la consulta.
+    if($result->num_rows > 0){ // Si devuelve más de 0 filas.
+      $row = $result->fetch_assoc(); // Guarda el resultado.
+      $this->conection->close(); // Cierra la conexión.
+      return new Note($row['id'], $row['titulo'], $row['descripcion']); // Devuelve un objeto nota con los datos de la BBDD.
+    } else { // Si no devuelve más de 0 filas.
+      $this->conection->close(); // Cierra la conexión.
+      return false; // Devuelve false.
     }
+  }
+
+  // Método para inserta una nota.
+  public function insertNote(string $titulo, string $descripcion){
+    $this->getConection(); // Inicializa la conexión.
+    $sql = "INSERT INTO ". $this->table ." (titulo, descripcion) VALUES ('$titulo', '$descripcion')"; // Consulta.
+    if($this->conection->query($sql) === TRUE){ // Si se inserta correctamente.
+      $id = $this->conection->insert_id; // Recoge el último id generado en la tabla.
+      $this->conection->close(); // Cierra la conexión.
+      return new Note($id, $titulo, $descripcion); // Devuelve un objeto nota con el último id generado en la bbdd y los atributos dados por el usuario.
+    } else { // Si no se inserta.
+      $this->conection->close(); // Cierra la conexión.
+      return false; // Devuelve false.
+    }
+  }
+
+  public function updateNote(int $id, string $titulo, string $descripcion){
+    $this->getConection(); // Inicializa la conexión.
+    $sql = "UPDATE ". $this->table ." SET titulo='$titulo', descripcion='$descripcion' WHERE id=$id"; // Consulta.
+    if($this->conection->query($sql) === TRUE){ // Si se actualiza correctamente.
+      $this->conection->close(); // Cierra la conexión.
+      return true; // Devuelve true.
+    } else { // Si no.
+      $this->conection->close(); // Cierra la conexión.
+      return false; // Devuelve false.
+    }
+  }
+
+  public function deleteNote($id){
+    $this->getConection(); //Inicializa la conexión.
+    $sql = "DELETE FROM ". $this->table ." WHERE id=$id"; // Consulta.
+    if($this->conection->query($sql) === TRUE){ // Si se borra correctamente.
+      $this->conection->close(); // Cierra la conexión.
+      return true; // Devuelve true.
+    } else { // Si no.
+      $this->conection->close(); // Cierra la conexión.
+      return false; // Devuelve false.
+    }
+  }
+
+}
+
 ?>
