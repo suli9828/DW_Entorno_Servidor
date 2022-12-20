b504082851bd04287507e97a59716c774e13f8ab
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-12-20 09:14:50 +0100

    Add files via upload

--- MVC/config/config.php
+++ MVC/config/config.php
@@ -1,12 +1,10 @@
 <?php
 
-// Valores de la Base de Datos.
-define("DB_HOST", "localhost"); // Servidor
-define("DB", "nota"); // Nombre de la Base de Datos.
-define("DB_USER", "root"); // Nombre de usuario gestor de la Base de Datos.
-define("DB_PASSWD", ""); // Contraseña del usuario.
+define("DB_HOST", "localhost"); 
+define("DB", "nota"); 
+define("DB_USER", "root"); 
+define("DB_PASSWD", ""); 
 
-// Acción por defecto para el index si no le llega nada.
 define("DEFAULT_ACTION", "list");
 
 ?>
